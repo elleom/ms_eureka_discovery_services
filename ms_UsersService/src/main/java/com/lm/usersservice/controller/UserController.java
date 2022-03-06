9aa6c33a128ba5e2e0e5ff4a993262459807241b
elleom <el_leo_m@hotmail.com>
2022-03-06 21:02:03 +0100

    print server.port

--- ms_UsersService/src/main/java/com/lm/usersservice/controller/UserController.java
+++ ms_UsersService/src/main/java/com/lm/usersservice/controller/UserController.java
@@ -1,5 +1,6 @@
 package com.lm.usersservice.controller;
 
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,8 +13,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/users")
 public class UserController {
 
+    private final Environment env;
+
+    public UserController(Environment env) {
+        this.env = env;
+    }
+
     @GetMapping("/status/check")
     public String getStatus(){
-        return "Online";
+
+        return "Online: \nport=" + env.getProperty("local.server.port");
     }
 }
