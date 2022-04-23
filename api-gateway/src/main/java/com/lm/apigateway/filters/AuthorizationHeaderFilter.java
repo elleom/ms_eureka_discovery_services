035dec0f67b2d0ed765274fd08055eff47e21470
elleom <el_leo_m@hotmail.com>
2022-04-23 22:37:53 +0200

    add actuator config for ms

--- api-gateway/src/main/java/com/lm/apigateway/filters/AuthorizationHeaderFilter.java
+++ api-gateway/src/main/java/com/lm/apigateway/filters/AuthorizationHeaderFilter.java
@@ -7,13 +7,11 @@ import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFac
 import org.springframework.core.env.Environment;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.server.reactive.ServerHttpResponse;
 import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.http.server.reactive.ServerHttpResponse;
 import org.springframework.stereotype.Component;
 import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Mono;
-
-import java.util.Optional;
 
 /**
  * @author el_le
