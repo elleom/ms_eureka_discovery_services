d1aaf95fa53bcd9fec15fe0ef4dbef9f19b8e4fc
elleom <el_leo_m@hotmail.com>
2022-05-24 20:39:14 +0200

    add Circuit Breaker with Feign

--- ms_UsersService/src/main/java/com/lm/usersservice/data/feign/AlbumsServiceClient.java
+++ ms_UsersService/src/main/java/com/lm/usersservice/data/feign/AlbumsServiceClient.java
@@ -1,19 +1,53 @@
 package com.lm.usersservice.data.feign;
 
 import com.lm.usersservice.ui.model.AlbumResponseModel;
+import feign.FeignException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.cloud.openfeign.FallbackFactory;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
  * @author el_le
  * @since 5/13/2022 11:52 AM
  */
-@FeignClient(name="albums-ws")
+@FeignClient(name = "albums-ws", fallbackFactory = AlbumsFallbackFactory.class)
 public interface AlbumsServiceClient {
 
     @GetMapping("/users/{id}/albums")
     public List<AlbumResponseModel> getAlbums(@PathVariable String id);
+}
+
+@Component
+class AlbumsFallbackFactory implements FallbackFactory<AlbumsServiceClient> {
+    @Override
+    public AlbumsServiceClient create(Throwable cause) {
+        return new AlbumsServiceClientFallback(cause);
+    }
+}
+
+class AlbumsServiceClientFallback implements AlbumsServiceClient {
+    Throwable cause;
+    Logger logger = LoggerFactory.getLogger(this.getClass());
+
+    public AlbumsServiceClientFallback(Throwable cause) {
+        this.cause = cause;
+    }
+
+    @Override
+    public List<AlbumResponseModel> getAlbums(String id) {
+        if (cause instanceof FeignException && ((FeignException) cause).status() == 404) {
+            logger.error("404 Error when getAlbums was called with userId: " +
+                    id + ". Error Message: " + cause.getLocalizedMessage());
+        } else {
+            logger.error("Other error took place: " + cause.getLocalizedMessage());
+        }
+        return new ArrayList<>();
+    }
 }
