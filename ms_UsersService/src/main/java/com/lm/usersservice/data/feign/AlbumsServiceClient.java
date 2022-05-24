b1aa84744f6c25e48807f04435013ff4242ab625
elleom <el_leo_m@hotmail.com>
2022-05-24 22:49:55 +0200

    remove hystrix and add resilience4J

--- ms_UsersService/src/main/java/com/lm/usersservice/data/feign/AlbumsServiceClient.java
+++ ms_UsersService/src/main/java/com/lm/usersservice/data/feign/AlbumsServiceClient.java
@@ -2,11 +2,8 @@ package com.lm.usersservice.data.feign;
 
 import com.lm.usersservice.ui.model.AlbumResponseModel;
 import feign.FeignException;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.cloud.openfeign.FallbackFactory;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
@@ -17,37 +14,14 @@ import java.util.List;
  * @author el_le
  * @since 5/13/2022 11:52 AM
  */
-@FeignClient(name = "albums-ws", fallbackFactory = AlbumsFallbackFactory.class)
+@FeignClient(name = "albums-ws")
 public interface AlbumsServiceClient {
 
     @GetMapping("/users/{id}/albums")
+    @CircuitBreaker(name="albums-ws", fallbackMethod = "getAlbumsFallBack")
     public List<AlbumResponseModel> getAlbums(@PathVariable String id);
-}
 
-@Component
-class AlbumsFallbackFactory implements FallbackFactory<AlbumsServiceClient> {
-    @Override
-    public AlbumsServiceClient create(Throwable cause) {
-        return new AlbumsServiceClientFallback(cause);
-    }
-}
-
-class AlbumsServiceClientFallback implements AlbumsServiceClient {
-    Throwable cause;
-    Logger logger = LoggerFactory.getLogger(this.getClass());
-
-    public AlbumsServiceClientFallback(Throwable cause) {
-        this.cause = cause;
-    }
-
-    @Override
-    public List<AlbumResponseModel> getAlbums(String id) {
-        if (cause instanceof FeignException && ((FeignException) cause).status() == 404) {
-            logger.error("404 Error when getAlbums was called with userId: " +
-                    id + ". Error Message: " + cause.getLocalizedMessage());
-        } else {
-            logger.error("Other error took place: " + cause.getLocalizedMessage());
-        }
+    default List<AlbumResponseModel> getAlbumsFallBack(String id, Throwable exception) {
         return new ArrayList<>();
     }
 }
